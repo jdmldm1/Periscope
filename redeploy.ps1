368cf8e59b1d23633cc60da8f0b016fdbad13a8f
jdmldm1 <145297886+jdmldm1@users.noreply.github.com>
2026-06-18 21:18:11 -0500

    fixing package image references due to repo rename

--- redeploy.ps1
+++ redeploy.ps1
@@ -35,44 +35,9 @@ if ($Nuke) {
     Write-Host "Zarf initialized. Cluster ready."
 }
 
-Write-Host "Building frontend..."
-Push-Location frontend
-npm run build
-Pop-Location
 
-function Build-Connected {
-    Write-Host "Building Connected docker image..."
-    docker build --build-arg CACHE_GRYPE_DB=false -t ghcr.io/jdmldm1/periscope-kubernetes:1.1.3-connected .
 
-    Write-Host "Creating Connected Zarf package..."
-    if (Test-Path .\zarf-package-periscope-amd64-1.1.3.tar.zst) {
-        Remove-Item .\zarf-package-periscope-amd64-1.1.3.tar.zst -Force
-    }
-    Invoke-Zarf package create --confirm
-}
 
-function Build-Airgap {
-    Write-Host "Building Airgap docker image..."
-    docker build --build-arg CACHE_GRYPE_DB=true -t ghcr.io/jdmldm1/periscope-kubernetes:1.1.3 .
-
-    Write-Host "Creating Airgap Zarf package..."
-    if (Test-Path .\zarf-package-periscope-airgap-amd64-1.1.3.tar.zst) {
-        Remove-Item .\zarf-package-periscope-airgap-amd64-1.1.3.tar.zst -Force
-    }
-
-    # Temporarily swap manifests for Airgap
-    Rename-Item -Path "zarf.yaml" -NewName "zarf-connected.tmp"
-    try {
-        Copy-Item -Path "zarf-airgap.yaml" -Destination "zarf.yaml"
-        Invoke-Zarf package create --confirm
-    }
-    finally {
-        if (Test-Path "zarf.yaml") {
-            Remove-Item -Path "zarf.yaml" -Force
-        }
-        Rename-Item -Path "zarf-connected.tmp" -NewName "zarf.yaml"
-    }
-}
 
 function Ensure-ZarfInit {
     $prev = $ErrorActionPreference
@@ -88,25 +53,17 @@ function Ensure-ZarfInit {
 }
 
 if ($Mode -eq "Connected") {
-    Build-Connected
+    
     Ensure-ZarfInit
     Write-Host "Deploying Connected Zarf package..."
-    Invoke-Zarf package deploy .\zarf-package-periscope-amd64-1.1.3.tar.zst --confirm
-
+    zarf package deploy oci://ghcr.io/jdmldm1/packages/periscope:1.1.3
     Write-Host "Restarting deployment..."
     kubectl rollout restart deployment periscope -n periscope
 }
 elseif ($Mode -eq "Airgap") {
-    Build-Airgap
     Ensure-ZarfInit
     Write-Host "Deploying Airgap Zarf package..."
-    Invoke-Zarf package deploy .\zarf-package-periscope-airgap-amd64-1.1.3.tar.zst --confirm
-
+    zarf package deploy oci://ghcr.io/jdmldm1/packages/periscope-airgap:1.1.3
     Write-Host "Restarting deployment..."
     kubectl rollout restart deployment periscope -n periscope
-}
-elseif ($Mode -eq "Both") {
-    Build-Connected
-    Build-Airgap
-    Write-Host "Finished compiling both Connected and Airgap Zarf packages."
 }
